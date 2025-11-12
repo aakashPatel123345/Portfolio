8172490177f725246984fc9be65ea8a4cf1c2617
Aakash Patel <apatel59@umd.edu>
2025-11-12 14:34:36 -0500

    removed vercel analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Projects from './components/sections/Projects'
 import Contact from './components/sections/Contact'
 import Footer from './components/sections/Footer'
 
-import { Analytics } from '@vercel/analytics/next'
+ // import { Analytics } from '@vercel/analytics/next'
 
 function App() {
   return (
